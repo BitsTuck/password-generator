c1fa43c971ff44372aeca3d1182b63ce98f658ce
Elizabeth Tucker <bits.tucker@gmail.com>
2023-09-26 19:23:16 -0400

    added character variables

--- script.js
+++ script.js
@@ -16,15 +16,22 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-function generatePassword(){
-  // All your code here
-// var numChar = window.prompt
-// console.log(numChar);
+// Start here
+function addEventListener(){
+  alert("Hello! Thank you for coming to the password generator. Please answer a few questions about the type of password you would like. To proceed, click OK.");
+  console.log("This function exists");
+}
 // var inclNums = window.confirm true/false
 
 // array for each character type
 // var numArray = ["0","1"]
+var upperCase = ["A","B","C","D","E","F","G","H", "I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 
+var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+
+var numbers = ["0","1","2","3","4","5","6","7","8","9"]
+
+var specChar = ["!","#","$","%","^","&","*","(",")","~","+"]
 // if statement
 // var options =[];
   // if true push/concat that array into a new array var
@@ -35,5 +42,4 @@ function generatePassword(){
   // var chosenArr = [];
   // turn array into string
 
-  return password;
-}
+  // return password;
