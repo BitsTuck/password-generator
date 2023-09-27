99fc0e26f38f77af5c5911fa8cc43b12d6010d49
Elizabeth Tucker <bits.tucker@gmail.com>
2023-09-27 18:29:38 -0400

    welcome prompt and number of characters prompt

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 // Assignment code here
+var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+
+var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+
+var numbers = ["0","1","2","3","4","5","6","7","8","9"]
+
+var specChar = ["!","#","$","%","^","&","*","(",")","~","+"]
+
+var numChar = []
 
 
+var startGenerator = window.confirm("Hello! Thank you for coming to the password generator. Please answer a few questions about the type of password you would like. To proceed, click OK."); 
+
+if (startGenerator== true) {
+  window.prompt ("Please select the number of characters you would like for your password (8-128).")
+} else {
+  alert ("Thanks for stopping by.")
+}  //Initial greeting/intro. Can proceed (true) or leave (false)
+
+
+
+//DO NOT PUSH THE BUTTON YET
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -17,21 +37,18 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 // Start here
-function addEventListener(){
-  alert("Hello! Thank you for coming to the password generator. Please answer a few questions about the type of password you would like. To proceed, click OK.");
-  console.log("This function exists");
-}
+
+Array.fill(number, number, 128)
+var charNum = []
+
+
+
+
+
 // var inclNums = window.confirm true/false
 
 // array for each character type
 // var numArray = ["0","1"]
-var upperCase = ["A","B","C","D","E","F","G","H", "I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-
-var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-
-var numbers = ["0","1","2","3","4","5","6","7","8","9"]
-
-var specChar = ["!","#","$","%","^","&","*","(",")","~","+"]
 // if statement
 // var options =[];
   // if true push/concat that array into a new array var
@@ -43,3 +60,9 @@ var specChar = ["!","#","$","%","^","&","*","(",")","~","+"]
   // turn array into string
 
   // return password;
+
+
+
+
+
+
