7db26f3142f145000d1afe0d6d70e768b4951366
Elizabeth Tucker <bits.tucker@gmail.com>
2023-09-28 20:38:05 -0400

    Final javascript with comments

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
-// Assignment code here
+//Variable arrays listing all of the possible characters to include in the password
 var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 var numbers = ["0","1","2","3","4","5","6","7","8","9"]
 var specialCharacter = ["!","#","$","%","^","&","*","(",")","~","+"]
+//This variable will hold the number of characters input by the user
 var numChar = [];
+//This variable will hold the characters selected by the user-- upperCase, lowerCase, numbers, specialCharacter-- in any combination.
 var charList = [];
+
+
+//This function starts the password generator and asks the user for the number of characters they would like their password to be. If a user selects a number less than 8 or greater than 128, they will be returned to the beginning of the function. If they cancel the request, they will receive a message thanking them for coming to the site.
 var letsStart = function() {
   var newUser = window.confirm("Hello! Thank you for coming to the password generator. Please answer a few questions about the type of password you would like. To proceed, click OK.");
   if (newUser) {
-    numChar = parseInt(window.prompt ("Please select the number of characters you would like for your password.", "Any whole number between 8 and 128"));
+    numChar = parseInt(window.prompt ("Please select the number of characters you would like for your password. Any whole number between 8 and 128."));
     console.log(numChar);
    } else {
      alert ("Thanks for stopping by.");
@@ -21,6 +26,7 @@ var letsStart = function() {
    
 letsStart();
 
+//This section takes the user through a series of confirm windows (that ask the user to click OK or Cancel) to gather Boolean data about what should be included in the password. If the OK button is clicked for any given criteria, that array is pushed to another variable (charList) that will hold the data. If the Cancel button is clicked for any given criteria, the data is not pushed to charList.
 var characters = function () {
   var capLet = window.confirm ("Would you like your password to have capital letters? Click OK for yes, Click Cancel for no.")
   var lowLet = window.confirm ("Would you like your password to have lowercase letters? Click OK for yes, Click Cancel for no.")
@@ -38,14 +44,15 @@ var characters = function () {
   } 
   if (specChar) {
     charList.push(...specialCharacter);
-  } 
+  }
   console.log(charList);
+
   }
   characters();
-
+//This function generates the password by looping back to the charList and grabbing random characters. The loop goes the same number of times as the number that the user selected for numChar. If the user selected a 90 character password, the function loops back 90 times. The password is then returned.
 
   function generatePassword() {
-    var password = ""
+    var password = " "
     for(var i = 0; i < numChar; i++) {
       var index = Math.floor(Math.random()*charList.length);
       password += charList[index];
@@ -53,52 +60,21 @@ var characters = function () {
     console.log(password);
     return password;
   }
+
+  //This code runs the generatePassword function, and then prompts the user in an alert window (OK button only) to click generate password to see what has been generated.
   var generated = generatePassword()
-  console.log("Generated Password" + generated)
 
+  window.alert("Thank you! Please push Generate Password to see your new password.")
 
-// function leroy(x, y){
-//   x = Math.sqrt(x)
-//   return x + y
-// }
+  //This was the code provided to us for this assignment. It pulls in the password variable that was created above, and pulls it into the HTML (#generate and #password ids) and holds the eventListener that displays the password after the Generate Password button is clicked.
+  
+  var generateBtn = document.querySelector("#generate");
+  function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+  
+    passwordText.value = password;
+  
+  }
+  generateBtn.addEventListener("click", writePassword);
 
-// const jenkins = leroy(5, 8)
-// console.log(jenkins)
-
-
-
-
-
-//DO NOT PUSH THE BUTTON YET
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-// Start here
-
-// var inclNums = window.confirm true/false
-
-// array for each character type
-// var numArray = ["0","1"]
-// if statement
-// var options =[];
-  // if true push/concat that array into a new array var
-
-// for loop through options array based on required characters
-  // Random index value for every loop
-  // Google this; How to randomly choose a value from an array
-  // var chosenArr = [];
-  // turn array into string
-
-  // return password;
